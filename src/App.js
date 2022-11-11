239dc4acdf209a83bb514c34b3c393158dbe6ec9
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2022-11-11 18:57:14 +0100

    RemoveTask interaction added

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import Header from "./Header";
 import Container from "./Container";
 import { useState } from "react";
 
-const tasks = [
-  { id: 1, content: "przejść na Reacta", done: true },
-  { id: 2, content: "zjeść obiadokolację", done: false },
-  { id: 3, content: "poukładać kostkę", done: false },
-];
-
 function App() {
   const [hideDone, setHideDone] = useState(false);
+  const [tasks, setTasks] = useState([
+    { id: 1, content: "przejść na Reacta", done: true },
+    { id: 2, content: "zjeść obiadokolację", done: false },
+    { id: 3, content: "poukładać kostkę", done: false },
+  ]);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
+  };
+
+  const removeTask = (id) => {
+    setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
   return (
@@ -37,7 +40,10 @@ function App() {
             toggleHideDone={toggleHideDone} />
         }
         body={
-          <Tasks tasks={tasks} hideDone={hideDone} />
+          <Tasks 
+          tasks={tasks} 
+          hideDone={hideDone} 
+          removeTask={removeTask} />
         }
 
       />
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const Tasks = ({ tasks, hideDone }) => (
+const Tasks = ({ tasks, hideDone, removeTask }) => (
     <ul className="tasks">
         {tasks.map(task => (
             <li
@@ -13,7 +13,10 @@ const Tasks = ({ tasks, hideDone }) => (
                 <span className={`tasks__item${task.done ? " tasks__item--done" : ""}`}>
                     {task.content}
                 </span>
-                <button className="tasks__button tasks__button--remove">
+                <button
+                    className="tasks__button tasks__button--remove"
+                    onClick={() => removeTask(task.id)}
+                >
                     🗑
                 </button>
             </li>
