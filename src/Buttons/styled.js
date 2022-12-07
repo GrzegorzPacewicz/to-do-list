33f35c39107164fde7b96491ec81afa939c431b9
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2022-12-07 14:51:49 +0100

    typo

--- src/Buttons/styled.js
+++ src/Buttons/styled.js
@@ -13,7 +13,7 @@ export const Button = styled.button`
   border: none;
   padding: 10px;
   background-color: transparent;
-  color: ${({ theme }) => theme.color.teal}
+  color: ${({ theme }) => theme.color.teal};
   transition: color 0.5s;
   margin: 0 0 0 20px;
 
