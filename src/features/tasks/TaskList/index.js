2f05f62682725f9c13d22626c7770748e38a9b35
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2022-12-16 14:03:47 +0100

    application split into features

--- src/features/tasks/TaskList/index.js
+++ src/features/tasks/TaskList/index.js
@@ -1,6 +1,6 @@
 import { List, Item, Content, ButtonToggleDone, ButtonRemove } from "./styled";
 
-const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
+const TaskList = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
   <List>
     {tasks.map((task) => (
       <Item key={task.id} hidden={task.done && hideDone}>
@@ -22,4 +22,4 @@ const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
   </List>
 );
 
-export default Tasks;
+export default TaskList;
---
+++ src/features/tasks/Tasks.js
@@ -0,0 +1,53 @@
+import Form from "./Form";
+import TaskList from "./TaskList";
+import Buttons from "./Buttons";
+import Section from "../../common/Section";
+import Header from "../../common/Header";
+import { Container } from "../../common/Container/styled";
+import { useTasks } from "../../useTasks";
+
+function Tasks() {
+  const {
+    tasks,
+    hideDone,
+    addNewTask,
+    removeTask,
+    setAllDone,
+    toggleTaskDone,
+    toggleHideDone,
+  } = useTasks();
+
+  return (
+    <Container>
+      <Header title="Lista zadań" />
+
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form addNewTask={addNewTask} />}
+      />
+
+      <Section
+        title="Lista zadań"
+        body={
+          <TaskList
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+            toggleHideDone={toggleHideDone}
+          />
+        }
+        extraHeaderContent={
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
+          />
+        }
+      />
+    </Container>
+  );
+}
+
+export default Tasks;
