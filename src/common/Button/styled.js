15d44d32476dd9ea02767cf777e36f0ca37fd752
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2022-12-27 13:44:45 +0100

    Button moved to common

---
+++ src/common/Button/styled.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  border: none;
+  padding: 10px;
+  background-color: transparent;
+  color: ${({ theme }) => theme.color.teal};
+  transition: color 0.5s;
+  margin: 0 0 0 20px;
+  cursor: pointer;
+
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    flex-basis: 100%;
+    margin: 0;
+  }
+
+  &:hover {
+    filter: brightness(110%);
+  }
+
+  &:active {
+    filter: brightness(120%);
+  }
+  &:disabled {
+    color: ${({ theme }) => theme.color.silver};
+    cursor: not-allowed;
+  }
+`;
