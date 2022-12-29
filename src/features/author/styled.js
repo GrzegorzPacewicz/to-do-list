d14500318cb0146703790e761f48911017c2a0d6
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2022-12-29 12:48:26 +0100

    Image in author page added

--- src/features/author/styled.js
+++ src/features/author/styled.js
@@ -1,7 +1,24 @@
 import styled from "styled-components";
 
-export const Paragraph = styled.p`
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Image = styled.img`
+  border-radius: 50%;
+  margin: 20px auto;
+  box-shadow: 5px 5px 10px #aab;
+`;
+
+export const Text = styled.p`
   line-height: 150%;
+  margin: 0 20px;
+  text-align: justify;
+`;
+
+export const Paragraph = styled.p`
+  padding: 0;
 `;
 
 export const Link = styled.a`
