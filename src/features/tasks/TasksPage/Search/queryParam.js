f6d9f08cb93c35dbca4dfd28794be8b3ac51375a
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2023-05-25 14:55:44 +0200

    Rearranged import in queryParam.js

--- src/features/tasks/TasksPage/Search/queryParam.js
+++ src/features/tasks/TasksPage/Search/queryParam.js
@@ -1,4 +1,4 @@
-import { useHistory, useLocation } from "react-router";
+import { useHistory, useLocation } from "react-router-dom";
 
 export const useQueryParam = (key) => {
   const location = useLocation();
